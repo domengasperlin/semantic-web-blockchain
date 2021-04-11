8f2cb08e4e507678558dd52f41a3e6b5e13df613
Domen Gasperlin <domen.gasperlin@gmail.com>
2021-04-11 22:24:45 +0200

    Output ontology to file

--- src/main/java/LoadFiles.java
+++ src/main/java/LoadFiles.java
@@ -1,5 +1,6 @@
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat;
+import org.semanticweb.owlapi.formats.TurtleDocumentFormat;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.model.parameters.Imports;
 
@@ -17,16 +18,18 @@ public class LoadFiles {
             e.printStackTrace();
         }
 
-        // ABox - facts
-        Set<OWLAxiom> aBoxAxioms = ontology.getABoxAxioms(Imports.INCLUDED);
         // TBox - classes and properties
         Set<OWLAxiom> tBoxAxioms = ontology.getTBoxAxioms(Imports.INCLUDED);
+        // ABox - facts, data
+        Set<OWLAxiom> aBoxAxioms = ontology.getABoxAxioms(Imports.INCLUDED);
         // RBox - reflexivity, symmetry and transitivity of roles
         Set<OWLAxiom> rBoxAxioms = ontology.getRBoxAxioms(Imports.INCLUDED);
 
 
         try {
             ontology.saveOntology(new FunctionalSyntaxDocumentFormat(), System.out);
+            File outputOntology = new File("cud_out.owl");
+            ontology.saveOntology(new TurtleDocumentFormat(), IRI.create(outputOntology.toURI()));
         } catch (OWLOntologyStorageException e) {
             e.printStackTrace();
         }
