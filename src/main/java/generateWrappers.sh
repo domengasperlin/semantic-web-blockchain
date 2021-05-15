e8b1a9f9f9cdb6075fb6685d5d359d3494dbb759
Domen Gasperlin <domen.gasperlin@gmail.com>
2021-05-15 18:04:46 +0200

    Parametrize shell script for generating smart contract wrappers

--- src/main/java/generateWrappers.sh
+++ src/main/java/generateWrappers.sh
@@ -1,2 +1,3 @@
-solc ../resources/Storage.sol --bin --abi --optimize -o ../resources/ --overwrite
+contractLocation=../resources/Storage.sol;
+solc $contractLocation --bin --abi --optimize -o ../resources/ --overwrite
 web3j generate solidity -b ../resources/Storage.bin -a ../resources/Storage.abi -o . -p contracts
