8b3981ae9a6af6cb71587dffcabaaac319c4e1a8
Domen Gasperlin <domen.gasperlin@gmail.com>
2021-09-03 02:05:42 +0200

    timing poc

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -8,8 +8,10 @@ public class Demo {
     private static final Logger log = Logger.getLogger(Demo.class.getName());
     public static Boolean useReasoner;
     public static Boolean isInitialLoad = true;
+    public static long programStarted = System.nanoTime();
 
     public static void main(String[] args) throws Exception {
+        int toMsConvert = 1000000;
         ConfigLoader configLoader = new ConfigLoader("src/main/java/config.yaml");
         ArrayList<String> inputOntologyFiles = (ArrayList<String>) configLoader.getOntology().get("loadFromFiles");
         String IPFSNodeAddress = (String) configLoader.getIPFS().get("nodeAddress");
@@ -30,7 +32,9 @@ public class Demo {
         IPFSHelpers ipfsHelpers = new IPFSHelpers(new IPFS(IPFSNodeAddress));
 
         // Load the ontology files into the Apache Jena
+        long startTimeJena = System.nanoTime();
         JenaHelpers jenaHelpers = new JenaHelpers(inputOntologyFiles, useReasoner, isInitialLoad);
+        log.info("Jena load: " + (System.nanoTime() - startTimeJena) / toMsConvert + " ms");
         // If exception is not thrown till this point and reasoner is active ontology is consistent and we can move on
         if (isInitialLoad) {
             // Upload ontology to IPFS
@@ -41,46 +45,74 @@ public class Demo {
             log.info("[IPFS upload content identifiers] " + inputOntologyCIDs);
 
             // Store IPFS content identifiers to Ethereum
+            long deployContractStart = System.nanoTime();
             ethereumHelpers.deployStorageContract();
+            log.info("Deploy contract: " + (System.nanoTime() - deployContractStart) / toMsConvert + " ms");
+
             for (String inputOntologyCID : inputOntologyCIDs) {
+                long contractMethodCallStart = System.nanoTime();
                 TransactionReceipt transaction = ethereumHelpers.getContract().addInputOntology(inputOntologyCID).send();
                 log.info("[ETH] transaction: " + transaction.getTransactionHash());
+                log.info("Contract method addInputOntology call: " + (System.nanoTime() - contractMethodCallStart) / toMsConvert + " ms");
             }
 
         } else {
             // Retrieve IPFS content identifiers from Ethereum
-            String smartContractAddress = "0x7da519706a64299801f533369a3e36cc7503bd98";
+            String smartContractAddress = "0xd9ff39f2395ebc672e187dcb2d30b82a4ca38570";
+            long ethereumOperationsStart = System.nanoTime();
             ethereumHelpers.loadContractAtAddress(smartContractAddress);
-
             BigInteger inputOntologiesLength = ethereumHelpers.getContract().getInputOntologiesLength().send();
-            for(BigInteger i = BigInteger.ZERO; i.compareTo(inputOntologiesLength) < 0; i = i.add(BigInteger.ONE)) {
+            log.info("ethereum size of input ontologies: " + (System.nanoTime() - ethereumOperationsStart) / toMsConvert + " ms");
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(inputOntologiesLength) < 0; i = i.add(BigInteger.ONE)) {
+                long ethereumGetInputOntologyStart = System.nanoTime();
                 String inputOntologyCID = ethereumHelpers.getContract().getInputOntology(i).send();
+                log.info("ethereum get input ontology: " + (System.nanoTime() - ethereumGetInputOntologyStart) / toMsConvert + " ms");
+
                 // TODO: Here we are making assumption that input files are the same
                 String inputOntologyPath = inputOntologyFiles.get(i.intValue());
                 // Download ontology from IPFS
+                long ipfsInputOnlogyStart = System.nanoTime();
                 ipfsHelpers.retrieveFileAndSaveItToLocalSystem(inputOntologyCID, inputOntologyPath);
+                log.info("IPFS get input ontology: " + (System.nanoTime() - ipfsInputOnlogyStart) / toMsConvert + " ms");
+
             }
             log.info("[IPFS downloaded and write to files]");
 
             // Download All SPARQL migrations
+            long ethereumGetSUPMigrationsStart = System.nanoTime();
             BigInteger sparqlMigrations = ethereumHelpers.getContract().getSUPMigrationsLength().send();
-            for(BigInteger i = BigInteger.ZERO; i.compareTo(sparqlMigrations) < 0; i = i.add(BigInteger.ONE)) {
+            log.info("ethereum getSUPMigrationsLength ontologies: " + (System.nanoTime() - ethereumGetSUPMigrationsStart) / toMsConvert + " ms");
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(sparqlMigrations) < 0; i = i.add(BigInteger.ONE)) {
+                long ethereumGetSUPMigrationStart = System.nanoTime();
                 String sparqlMigrationCID = ethereumHelpers.getContract().getSUPMigration(i).send();
+                log.info("ethereum getSUPMigration: " + (System.nanoTime() - ethereumGetSUPMigrationStart) / toMsConvert + " ms");
+
                 String sparqlMigrationLocation = sparqlMigrationDirectory.replace("$CID", sparqlMigrationCID);
+
+                long ipfsRetrieveMigrationStart = System.nanoTime();
                 ipfsHelpers.retrieveFileAndSaveItToLocalSystem(sparqlMigrationCID, sparqlMigrationLocation);
+                log.info("ipfs retrieve migration: " + (System.nanoTime() - ipfsRetrieveMigrationStart) / toMsConvert + " ms");
+
+                long sparqlMigrationStart = System.nanoTime();
                 jenaHelpers.executeSPARQLMigrationForDBSync(sparqlMigrationLocation);
+                log.info("sparql migration: " + (System.nanoTime() - sparqlMigrationStart) / toMsConvert + " ms");
             }
         }
 
         for (String query : SPARQLQueries) {
+
+            long executeQueryStart = System.nanoTime();
             log.info("[Executing SPARQL from file (.rq)] " + query);
             Boolean successful = jenaHelpers.executeSPARQL(query, inputOntologyFiles, ipfsHelpers, ethereumHelpers);
             if (!successful) {
                 log.severe("SPARQL query wasn't applied to the dataset");
                 break;
             }
+            log.info("Execute sparql query: " + (System.nanoTime() - executeQueryStart) / toMsConvert + " ms");
             // jenaHelpers.printDatasetToStandardOutput();
         }
+        log.info("Program finished in: " + (System.nanoTime() - programStarted) / toMsConvert + " ms");
+
     }
 
 }
